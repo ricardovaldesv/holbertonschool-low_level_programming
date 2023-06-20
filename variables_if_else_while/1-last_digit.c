30904044a78213a931740c11bd7c90daaed19aba
ricardovaldesv <ricardovaldes@icloud.com>
2023-06-20 12:52:25 -0700

    add program C The las digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: The last digit
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	d = n % 10
+	if (d > 5)
+		printf("Last digit %i and %i  is less grater than 5\n", n, d);
+	else if (n == 0)
+		printf("Last digit %i and is 0\n", n, d);
+	else
+		printf("Last digit %i and is less than 6 and not 0\n", n, d);
+	return (0);
+}
