da566303c0641875a83db6d974d6ed25d3e52578
ricardovaldesv <ricardovaldes@icloud.com>
2023-06-20 10:53:13 -0700

    add code C positive and negarive

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Description: 'Positive anything is better than negative nothing'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+if (n > 0)
+	printf("%i is positive\n", n);
+else if (n == 0)
+	printf("%i is zero\n", n);
+else
+	printf("%i is negative\n", n);
+return (0);
+}
