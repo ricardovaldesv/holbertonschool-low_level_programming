a5a97abd6b56d3fc85ee68b0cd6a7073101647a1
ricardovaldesv <ricardovaldes@icloud.com>
2023-06-23 16:10:07 -0700

    add function print  0 to 9 not 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,8 @@
 
 /**
  * print_most_numbers - This function print the numbers, from 0 to 9,
- *do not print 2 and 4 
+ *do not print 2 and 4
+ *
  */
 void print_most_numbers(void)
 
@@ -11,7 +12,7 @@ void print_most_numbers(void)
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
-		if (i != '2' && i !='4')
+		if (i != '2' && i != '4')
 		_putchar(i);
 	_putchar('\n');
 }
