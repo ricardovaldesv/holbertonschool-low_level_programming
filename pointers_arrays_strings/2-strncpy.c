c993bfd15732b1cfd85338fff2b915e76bacd6d0
ricardovaldesv <ricardovaldes@icloud.com>
2023-06-29 10:26:38 -0700

    add function copy a string v2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,15 +14,19 @@
 char *_strncpy(char *dest, char *src, int n)
 
 {
-	int i, j;
+	int i;
 
-	j = 0;
 	i = 0;
 
-	while (j < n)
+	while (src[i] != '\0' && i < n)
 	{
-		dest[i] = src[j];
-		j++;
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 
