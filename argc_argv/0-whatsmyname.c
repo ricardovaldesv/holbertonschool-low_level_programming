0dbf709992fe5fffafc3f7c50c131ba3529d2095
ricardovaldesv <ricardovaldes@icloud.com>
2023-07-07 12:19:17 -0700

    add program print a name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - This funtion print a name.
+ *
+ * @argc : argument
+ * @argv : array of pointer to string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	printf("%s\n", argv[0]);
+return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+0-whatsmyname.c : program that prints its name, followed by a new line.
