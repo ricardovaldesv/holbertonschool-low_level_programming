f11605d0c956df14bd5ad4f8a4a1c146f8ee6241
ricardovaldesv <ricardovaldes@icloud.com>
2023-07-10 16:57:42 -0700

    add function that copy a string in newly memory v3

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -15,17 +15,20 @@ char *_strdup(char *str)
 	char *copy;
 	unsigned int i, j;
 
+	if (str == NULL)
+		return (NULL);
+
 	i = 0;
 	while (str[i] != '\0')
 		i++;
 
 	copy = malloc(sizeof(char) * (i + 1));
 
+	if (copy == NULL)
+		return (NULL);
+
 	for (j = 0; j <= i; j++)
 		copy[j] = str[j];
-
-	if (str == NULL)
-		return (NULL);
 
 	return (copy);
 }
