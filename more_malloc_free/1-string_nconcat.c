3769c8049cc4b6eee4ad65b2d2525005c3968d15
ricardovaldesv <ricardovaldes@icloud.com>
2023-07-11 18:18:57 -0700

    add function that concatenates two string

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+
+/**
+ * string_nconcat - This funtion concatenates two string,
+ * which contains s1, followed by the first n bytes of s2, and null terminated
+ *
+ * @s1  : is a pointer of string to one
+ * @s2  : is a pointer of string to two
+ * @n : is a n bytes of s2
+ *
+ * Return: Pointer @copy to contatenates strings
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+{
+	char *copy;
+	unsigned int i, j, k;
+
+	if (s1 == NULL && s2 == NULL)
+		{
+		copy = malloc(sizeof(char) * 1);
+		copy[0] = '\0';
+		return (copy);
+		}
+
+		if (s1 == NULL)
+			i = 0;
+			else
+			{
+				i = 0;
+				while (s1[i] != '\0')
+				i++;
+			}
+		if (s2 == NULL)
+			j = 0;
+			else
+			{
+				j = 0;
+				while (s2[j] != '\0')
+				j++;
+			}
+		if (n > j)
+		copy = malloc(sizeof(char) * (i + j + 1));
+		copy = malloc(sizeof(char) * (i + n + 1));
+
+		if (copy == NULL)
+			return (NULL);
+		for (k = 0; k < i; k++)
+			copy[k] = s1[k];
+		if (j == 0)
+			copy[k] = s1[k];
+		else
+			if (n > j)
+			{
+				for (k = i; k <= (i + j); k++)
+					copy[k] = s2[k - i];
+			}
+			else
+				for (k = i; k <= (i + n); k++)
+					copy[k] = s2[k - i];
+		return (copy);
+}
