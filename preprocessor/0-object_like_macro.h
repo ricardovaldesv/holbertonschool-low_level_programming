116cedf271abf271898244e3ba3801e7eefe8dba
ricardovaldesv <ricardovaldes@icloud.com>
2023-07-13 09:11:22 -0700

    add header file with macro SIZE

---
+++ preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#define SIZE 1024
+
+/*
+ * File: 0-object_like_macro.h
+ * Author: Ricardo Valdes
+ * Description: Header file of macro 0-object_like_macro.h
+ */
---
+++ preprocessor/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: Ricardo Valdes
+ * Description: Header file of  prototypes for functions
+ */
+
+int _putchar(char c);
+
+#endif
