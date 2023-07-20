7fbc6c1fb90beeb049d0d94c17b580bb29965421
ricardovaldesv <ricardovaldes@icloud.com>
2023-07-20 09:09:12 -0700

    function that rturn sum

---
+++ variadic_functions/README.md
@@ -0,0 +1,1 @@
+0-sum_them_all.c : function that returns the sum of all its parameters.
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,19 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/*
+ * File: main.h
+ * Author: Ricardo Valdes
+ * Description: Header file of  prototypes for functions
+ */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
