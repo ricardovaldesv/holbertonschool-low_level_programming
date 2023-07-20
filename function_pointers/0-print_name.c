f487de5827d2c861e4f8d4fe514b77c4892eddcc
ricardovaldesv <ricardovaldes@icloud.com>
2023-07-19 20:40:50 -0700

    add function print name 2

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * print_name - This funtion prints a name
  * @name : pointer to name
@@ -9,6 +9,8 @@
 void print_name(char *name, void (*f)(char *))
 
 {
+	if (!name || !f)
+		return;
 	f(name);
 
 }
