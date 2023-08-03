981e9b7e1281c729ee19842bb8cd7e4f54f6cb67
ricardovaldesv <ricardovaldes@icloud.com>
2023-08-03 12:37:04 -0700

    function return nodes

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that retun number of nodes
+ * @h: pointer to list
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h);
+
+{
+	size_t i = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
