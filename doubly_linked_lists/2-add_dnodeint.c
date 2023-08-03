d7715d38a1e8ddb76bf2201439eec05c5bee5e5b
ricardovaldesv <ricardovaldes@icloud.com>
2023-08-03 15:23:37 -0700

    function add note at the beginning

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function that adds a new node
+ * at the beginning of a dlistint_t list.
+ * @head : pointer to list
+ * @n: data
+ * Return: the number of nodes
+ */
+
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+
+{
+	dlistint_t *newNode;
+
+	newNode = malloc(sizeof(dlistint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = *head;
+	newNode->prev = NULL;
+	if ((*head) != NULL)
+		(*head)->prev = newNode;
+	*head = newNode;
+}
