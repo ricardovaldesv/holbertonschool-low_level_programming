7b4ac757c11e95392d338d1351be049d19b8edbc
ricardovaldesv <ricardovaldes@icloud.com>
2023-08-04 15:06:18 -0700

    function 1

--- bit_manipulation/_putchar.c
+++ bit_manipulation/_putchar.c
@@ -1,19 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-/*
- * File: main.h
- * Author: Ricardo Valdes
- * Description: Header file of  prototypes for functions
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int _putchar(char c);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-
-#endif
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
