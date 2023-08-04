77a093dcd38e1bf83abab6146bb72d03bca5578d
ricardovaldesv <ricardovaldes@icloud.com>
2023-08-04 09:07:23 -0700

    function 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,43 +11,66 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp1;
-	dlistint_t *temp2;
-	unsigned int i;
+    struct dlistint_s *curNode;
+    struct dlistint_s *NodeToDel;
+    struct dlistint_s *ennode;
+    unsigned int i;
+    
+    size_t nodes = 0;
+    nodes = dlistint_len(*head);
+    
+    ennode = get_dnodeint_at_index(*head,nodes);
+ 
+    curNode = *head;
+    
+    for(i=0; i<index && curNode!=NULL; i++)
+    {
+        curNode = curNode->next;
+    }
+ 
+     if(index == 0)
+     {
+        struct dlistint_s * NodeToDel;
+        if(*head == NULL)
+        {
+         return (-1);
+        }
+        else
+        {
+         NodeToDel = *head;
+         *head = (*head)->next; 
+         (*head)->prev = NULL; 
+         free(NodeToDel);
+        }
+      }
+    
+    else if(curNode == ennode)
+    {
 
-	temp1 = *head;
+     if(ennode == NULL)
+     {
+       return (-1);
+     }
+     else
+     {
+        NodeToDel = ennode;
+        ennode = ennode->prev;
+        ennode->next = NULL;
+        free(NodeToDel);
+     }
 
-	if (temp1 != NULL)
-		while (temp1->prev != NULL)
-			temp1 = temp1->prev;
-
-	i = 0;
-
-	while (temp1 != NULL)
-	{
-		if (i == index)
-		{
-			if (i == 0)
-			{
-				*head = temp1->next;
-				if (*head != NULL)
-					(*head)->prev = NULL;
-			}
-			else
-			{
-				temp2->next = temp2->next;
-
-				if (temp1->next != NULL)
-					temp1->next->prev = temp2;
-			}
-
-			free(temp1);
-			return (1);
-		}
-		temp2 = temp1;
-		temp1 = temp1->next;
-		i++;
-	}
-
-	return (-1);
-}
+    }
+    
+    else if (curNode != NULL)
+    {
+        curNode->prev->next = curNode->next;
+        curNode->next->prev = curNode->prev;
+ 
+        free(curNode);
+    }
+    else
+    {
+        return (-1);
+    }
+return(1);
+} 
