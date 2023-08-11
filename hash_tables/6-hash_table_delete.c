b24cdee981b2a6892fb24338234a6f834ca5a775
ricardovaldesv <ricardovaldes@icloud.com>
2023-08-11 13:45:13 -0700

    function 6 v1

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that deletes a hash table.
+ *
+ * @ht: is the hash table
+ */
+
+
+void hash_table_delete(hash_table_t *ht)
+{
+	if (!ht)
+		return;
+
+	unsigned long int i;
+	hash_node_t *p, *current, *next;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		p = ht->array[i];
+		while (p != NULL)
+		{
+			current = p;
+			next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			p = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
